0f429d0765cfcecee957f4ed4d51ad31ffaab44c
julia <mercedes--2014@hotmail.com>
2017-09-12 12:26:57 -0400

    Add files via upload

--- models.py
+++ models.py
@@ -10,9 +10,9 @@ db = SQLAlchemy()
 class User(db.Model):
 	__tablename__='users'
 	uid=db.Column(db.Integer, primary_key=True)
-	firstname=db.Column(db.String(100))
-	lastname=db.Column(db.String(100))
-	email=db.Column(db.String(120), unique=True)
+	firstname=db.Column(db.String(50))
+	lastname=db.Column(db.String(50))
+	email=db.Column(db.String(100), unique=True)
 	pwdhash=db.Column(db.String(54))
 
 	def __init__(self, firstname, lastname, email, password):
@@ -33,8 +33,8 @@ class Place(object):
     # 80 meters is one minute walking time
     return int(meters / 80)  
 
-  def wiki_path(self, slug):
-    return urllib.parse.urljoin("http://en.wikipedia.org/wiki/", slug.replace(' ', '_'))
+  def wiki_path(self, s):
+    return urllib.parse.urljoin("http://en.wikipedia.org/wiki/", s.replace(' ', '_'))
   
   def address_to_latlng(self, address):
     g = geocoder.google(address)
@@ -44,30 +44,30 @@ class Place(object):
     lat, lng = self.address_to_latlng(address)
     
     query_url = 'https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord={0}%7C{1}&gslimit=8&format=json'.format(lat, lng)
-    g = urllib.request.urlopen(query_url)
-    results = g.read()
-    g.close()
+    d = urllib.request.urlopen(query_url)
+    json_data = d.read()
+    d.close()
 
-    data = json.loads(results)
+    data = json.loads(json_data)
     
     places = []
     for place in data['query']['geosearch']:
-      name = place['title']
+      place_name = place['title']
       meters = place['dist']
       lat = place['lat']
       lng = place['lon']
 
-      wiki_url = self.wiki_path(name)
+      wiki_url = self.wiki_path(place_name)
       walking_time = self.meters_to_walking_time(meters)
 
-      d = {
-        'name': name,
+      place_dict = {
+        'name': place_name,
         'url': wiki_url,
         'time': walking_time,
         'lat': lat,
         'lng': lng
       }
 
-      places.append(d)
+      places.append(place_dict)
 
     return places
